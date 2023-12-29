535098fe1d6ea42985de52eee9589a402ebb428c
Luis Neto <luis.neto.dev@gmail.com>
2023-12-29 16:49:30 +0000

    add graceful shutdown and logging

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,15 @@
-use axum::routing::{get, post};
-use axum::Router;
-use tokio::net::TcpListener;
+use std::time::Duration;
+
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use tokio::signal;
+use tokio::{net::TcpListener};
+use tower_http::timeout::TimeoutLayer;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
 pub struct Server {}
 
 impl Server {
@@ -9,12 +18,50 @@ impl Server {
     }
 
     pub async fn serve(&self) -> anyhow::Result<()> {
+        tracing_subscriber::registry()
+            .with(
+                tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                    "example_graceful_shutdown=debug,tower_http=debug,axum=trace".into()
+                }),
+            )
+            .with(tracing_subscriber::fmt::layer().without_time())
+            .init();
         let router = Router::new()
             .route("/:file_name", get(get_handler))
-            .route("/:file_name", post(post_handler));
+            .route("/:file_name", post(post_handler))
+            .layer((
+                TraceLayer::new_for_http(),
+                // Graceful shutdown will wait for outstanding requests to complete. Add a timeout so
+                // requests don't hang forever.
+                TimeoutLayer::new(Duration::from_secs(10)),
+            ));
         let listener = TcpListener::bind("127.0.0.1:3000").await?;
         axum::serve(listener, router)
-            .await.map_err(Into::into)
+            .with_graceful_shutdown(Server::shutdown())
+            .await
+            .map_err(Into::into)
+    }
+
+    async fn shutdown() {
+        let ctrl_c = async {
+            signal::ctrl_c().await.expect("Expecting CTRL+C");
+        };
+
+        #[cfg(unix)]
+        let terminate = async {
+            signal::unix::signal(signal::unix::SignalKind::terminate())
+                .expect("failed to install signal handler")
+                .recv()
+                .await;
+        };
+
+        #[cfg(not(unix))]
+        let terminate = std::future::pending::<()>();
+
+        tokio::select! {
+           _ = ctrl_c => {},
+           _ = terminate => {},
+        }
     }
 }
 

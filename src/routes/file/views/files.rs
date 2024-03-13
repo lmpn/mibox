d6b8e206e510d4a3616de90d13344cf5abebffc9
Luis Neto <luis.neto.dev@gmail.com>
2024-03-13 11:37:16 +0000

    infinite scroll

--- src/routes/file/views/files.rs
+++ src/routes/file/views/files.rs
@@ -1,13 +1,19 @@
+use anyhow::Context;
 use askama::Template;
 use axum::{
     extract::{Query, State},
     response::Html,
 };
+use itertools::Itertools;
 
-use crate::startup::AppState;
+use crate::{domain::drive::Drive, startup::AppState};
 #[derive(Template)]
 #[template(path = "files.html")]
-struct FilesTemplate {}
+struct FilesTemplate {
+    files: Vec<(usize, String)>,
+    count: usize,
+    offset: usize,
+}
 
 #[derive(serde::Deserialize)]
 pub struct ListQueryParams {
@@ -18,8 +24,23 @@ pub async fn files(
     Query(ListQueryParams { count, offset }): Query<ListQueryParams>,
     State(state): State<AppState>,
 ) -> Html<String> {
-    let files = FilesTemplate {};
-    let _c = count;
-    let _o = offset;
+    let files = if let Ok(files) = Drive::new("tmp".to_string())
+        .list(count, offset)
+        .await
+        .context("failed to list file")
+    {
+        files
+            .iter()
+            .enumerate()
+            .map(|(i, e)| (i + count + offset - 10, e.to_owned()))
+            .collect_vec()
+    } else {
+        vec![]
+    };
+    let files = FilesTemplate {
+        files,
+        count,
+        offset: count + offset,
+    };
     Html(files.render().unwrap())
 }
